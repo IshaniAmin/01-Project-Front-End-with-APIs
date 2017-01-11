72d419ded00ccd9f913c96b39d35fd0340c1851f
alinton12 <lintonarron@gmail.com>
2017-01-10 20:11:21 -0500

    registration page edits

--- assets/javascript/registration.js
+++ assets/javascript/registration.js
@@ -1,17 +1,3 @@
-// Initialize Firebase
-    var config = {
-         apiKey: "AIzaSyARm7xKPSKNRunk49DwplrL7Sb3mA0wTa4",
-         authDomain: "project-01-front-end-wit-39454.firebaseapp.com",
-         databaseURL: "https://project-01-front-end-wit-39454.firebaseio.com",
-         storageBucket: "project-01-front-end-wit-39454.appspot.com",
-         messagingSenderId: "780675474319"
-    };
-    firebase.initializeApp(config);
-    //Variables
-    //Get a reference to the database service
-    var database = firebase.database();
-
-
 var user_name = "";
 var user_email = "";
 var user_position = "";
@@ -23,17 +9,31 @@ var user_fav_songs = [];
 var song_id = "";
 var song_name = "";
 var song_url = "";
-var song_picture = "";   
+var song_picture = "";
+
+// Initialize Firebase
+    var config = {
+         apiKey: "AIzaSyARm7xKPSKNRunk49DwplrL7Sb3mA0wTa4",
+         authDomain: "project-01-front-end-wit-39454.firebaseapp.com",
+         databaseURL: "https://project-01-front-end-wit-39454.firebaseio.com",
+         storageBucket: "project-01-front-end-wit-39454.appspot.com",
+         messagingSenderId: "780675474319"
+    };
+    firebase.initializeApp(config);
+    //Variables
+    //Get a reference to the database service
+    var database = firebase.database();   
 
 // Capture Button Click
 $("#submit-playlist").on("click", function(event) {
-	//change the id tags - use all your inputs
+	
 	event.preventDefault();
 	name = $("#profile-name").val().trim();
 	email = $("#profile-email").val().trim();
 	position = $("#profile-position").val().trim();
 	interest = $("#profile-interests").val().trim();
 	fav_spot = $("#profile-fav-spot").val().trim();
+	fav_song = $("#profile-fav-song").val().trim();
 
 
 	// Code for the push
@@ -44,39 +44,33 @@ $("#submit-playlist").on("click", function(event) {
 			// user_profile_picture : profile,
 			user_interest : interest, 
 			user_fav_spot: fav_spot,
-			// user_fav_songs: songs,    
+			user_fav_songs: fav_song,    
 	  	dateAdded: firebase.database.ServerValue.TIMESTAMP
 	});
-
-	// database.ref('songs/').push({
-	// 	song_id: id,
-	// 	song_name: name,
-	// 	song_url: url,
-	// 	song_picture: picture
-
-	// })
 });
+
+
 
 $(document).ready( function() {
 
-	var songList = [];
+	// var songList = [];
 
-	function renderSongs(event) {
+	// function renderSongs(event) {
 
-		$(".table tbody").empty();
+	// 	$(".table tbody").empty();
 
-		for (var j=0; j < songList.length; j++) {
-		var songDiv = $("<tr class='item'>");
+	// 	for (var j=0; j < songList.length; j++) {
+	// 	var songDiv = $("<tr class='item'>");
 
-		var newAdd = $("<iframe class='song'>").attr("src", songList[j]).attr("frameborder", 0).attr("allowtransparency", true);
+	// 	var newAdd = $("<iframe class='song'>").attr("src", songList[j]).attr("frameborder", 0).attr("allowtransparency", true);
 
-		songDiv.append(newAdd);
+	// 	songDiv.append(newAdd);
 
-		$(".table tbody").prepend(songDiv);
+	// 	$(".table tbody").prepend(songDiv);
 
-		}
+	// 	}
 
-	}
+	// }
 
 	function displayResults(event) {
 
@@ -98,19 +92,18 @@ $(document).ready( function() {
         .done(function(response) {
 
         	console.log(response);
-        	var data = response.tracks;
-
-        	var tracks = $("<div>");
+        	var data = response.tracks
 
         	for(var i = 0; i < data.items.length; i++) {
+        	var tracks = $("<tr id='track'>");
 
-        		var name = "<br>" + data.items[i].name + "<br>";	
+        		var songName = "<br>" + data.items[i].name + "<br>";	
 
         		var albumArt = data.items[i].album.images[0].url;
 
-        		var image = $("<img class='img'>").attr("src", albumArt);
+        		var image = $("<img class='img-rounded' width='150px' height='150px'>").attr("src", albumArt);
         		var p = $("<p class='songInfo'>")
-        		p = name;
+        		p = songName;
 
         		var uri = data.items[i].uri;
 
@@ -120,11 +113,11 @@ $(document).ready( function() {
 
         		var playButton = $("<iframe class='preview'>").attr("src", preview).attr("frameborder", 1).attr("allowtransparency", true);
 
-        		var playlistAdd = $("<button class='add'>").text("Add To Playlist");
+        		var playlistAdd = $("<button type='button' class='add'>").text("Add To Playlist");
 
-        		tracks.append(name);
+        		tracks.append("<td class='col-md-2'>" + songName + "</td>");
         		tracks.append(image);
-        		tracks.append(playButton);
+        		tracks.append("<td class='col-md-5'>" + playButton);
         		tracks.append(playlistAdd);
 
         		// $(".song").on("load", function preventAutoPlay(event) {
@@ -132,18 +125,18 @@ $(document).ready( function() {
         		// 	audio.pause()
         		// })
 
-        		$("#results").prepend(tracks);
+        		$(".table tbody").prepend(tracks);
         	}
 
 
-        function playPreview(snd) {
-        	audio = new Audio(snd)
-        	audio.play();
-        }
+        // function playPreview(snd) {
+        // 	audio = new Audio(snd)
+        // 	audio.play();
+        // }
 
-        function pausePreview(snd) {
-        	audio.pause();
-        }
+        // function pausePreview(snd) {
+        // 	audio.pause();
+        // }
 
         // $(".img").on("click", function() {
 
@@ -158,24 +151,31 @@ $(document).ready( function() {
 	       //   	if(preview =! "") {
 	       //   		$(".img").on("click", ".img", pausePreview)
 	       //   	}
+	    // Playlist add button
+		$(".add").on("click", function () {
 
-	     $(".add").on("click", function (event) {
-	     	event.preventDefault();
+				var index = ($(this).index('.add'));
+				console.log(index)
+				id = data.items[index].id;
+				url = data.items[index].external_urls.spotify;
+				name = data.items[index].name;
+				
+				picture = data.items[index].album.images[0].url;
+				     	
+				// Code for the push
+				database.ref('songs/').push({
+					song_id: id,
+					song_name: name,
+					song_url: url,
+					song_picture: picture
 
-	     	var index = ($(this).index('.add'));
-
-	     	var uri = data.items[index].uri;
-
-	     	var track = "https://embed.spotify.com/?uri=" + uri;
-	     	songList.push(track);
-	     	renderSongs();
-	     	$("form")[0].reset();
-	     	$("#results").empty();
-
-	     	})
-
+				})
+			});	
+	     
         })
      }
  
-	$(document).on("click", ".btn", displayResults);		
+$(document).on("click", "#search", displayResults);	
+
+
 });
