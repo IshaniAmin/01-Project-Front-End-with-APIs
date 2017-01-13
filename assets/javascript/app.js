fd7b8972b6f4ffabf5211fced76bb0b1133b9f67
Ishani Amin <ishaniamin@Ishanis-MacBook-Pro.local>
2017-01-13 14:23:00 -0500

    added divisions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,47 +18,47 @@ $(document).ready(function(){
 	var database = firebase.database();
 //---------------------------------------------------------------------------------------
 
-
+//---------------------------------------------------------------------------------------
   database.ref('/users').on("value", function(data){
 
-    $(".list-group-item").on("click", function(event) {
+      $(".list-group-item").on("click", function(event) {
 
-        $("#cardTitle1").empty();
-        $("#email").empty();
-        $("#position").empty();
-        $("#interests").empty();
-        $("#favSpot").empty();
-        $("#addSongs").empty();
-  
-        var currentKey = $(this).attr("data-key");
-          console.log(currentKey);
+          $("#cardTitle1").empty();
+          $("#email").empty();
+          $("#position").empty();
+          $("#interests").empty();
+          $("#favSpot").empty();
+          $("#addSongs").empty();
+    
+          var currentKey = $(this).attr("data-key");
+            console.log(currentKey);
 
-          //------------------------------------------------------------------
-          database.ref('users/' + currentKey).on('value', function(data) {
+            //------------------------------------------------------------------
+            database.ref('users/' + currentKey).on('value', function(data) {
 
-            $("#cardTitle1").append("Name: " + data.val().user_name);
-            $("#email").append("Email: " + data.val().user_email);
-            $("#position").append("Position: " + data.val().user_position);
-            $("#interests").append("Interest: " + data.val().user_interest);
-            $("#favSpot").append("Favorite Spot: " + data.val().user_fav_spot);
-
-
-            for (var i=0; i < data.val().user_fav_songs.length; i++) {
-              $("#addSongs").append(data.val().user_fav_songs[i] + "<br>");
-            }
-
-            event.preventDefault();
-
-            });
-          //------------------------------------------------------------------
-
-    });
+              $("#cardTitle1").append("Name: " + data.val().user_name);
+              $("#email").append("Email: " + data.val().user_email);
+              $("#position").append("Position: " + data.val().user_position);
+              $("#interests").append("Interest: " + data.val().user_interest);
+              $("#favSpot").append("Favorite Spot: " + data.val().user_fav_spot);
 
 
+              for (var i=0; i < data.val().user_fav_songs.length; i++) {
+                $("#addSongs").append(data.val().user_fav_songs[i] + "<br>");
+              }
+
+              event.preventDefault();
+
+              });
+            //------------------------------------------------------------------
+      });
+//---------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------
     function results() {
-  
+    
           var artistInput = $("#query").val().trim();
-                
+                  
           // http://app.ticketmaster.com/discovery/v1/events.json?keyword=ariana&apikey=zyleKSfADiALla0NEoeit7ieP42ITTfA&callback=myFunction
 
           var queryURL = "http://app.ticketmaster.com/discovery/v1/events.json?keyword="+ artistInput +"&apikey=zyleKSfADiALla0NEoeit7ieP42ITTfA&callback=myFunction"
@@ -66,7 +66,7 @@ $(document).ready(function(){
           $.ajax({
               url: queryURL,
               method: "GET"
-             }).done(function(response) {
+              }).done(function(response) {
 
               var div = $("<div>");
 
@@ -88,11 +88,14 @@ $(document).ready(function(){
       return false;
 
       };
+//---------------------------------------------------------------------------------------
 
-  $(document).on("click", "#search", results);
-
+//---------------------------------------------------------------------------------------
+    $(document).on("click", "#search", results);
+//---------------------------------------------------------------------------------------
   
-  });
+  }); //ends the "database.ref('/users').on("value", function(data)"
+
 
 // Puts users' names into buttons dynamically
 //---------------------------------------------------------------------------------------
