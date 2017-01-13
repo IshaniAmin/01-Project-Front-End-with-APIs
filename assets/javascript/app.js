c045cb8c88b4e549e97f634d99a6b93ed59ba6ad
Ishani Amin <ishaniamin@Ishanis-MacBook-Pro.local>
2017-01-13 01:00:38 -0500

    added ticketmaster search engine to profile page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){      	
+$(document).ready(function(){  
 
   // Initialize Firebase
 	var config = {
@@ -14,81 +14,86 @@ $(document).ready(function(){
 	//Get a reference to the database service
 	var database = firebase.database();
 
-    database.ref('/users').on("value", function(data){
+  database.ref('/users').on("value", function(data){
 
-       $(".list-group-item").on("click", function(event) {
+    $(".list-group-item").on("click", function(event) {
 
-          $("#cardTitle1").empty();
-          $("#email").empty();
-          $("#position").empty();
-          $("#interests").empty();
-          $("#favSpot").empty();
-          $("#addSongs").empty();
-
-          event.preventDefault();
+        $("#cardTitle1").empty();
+        $("#email").empty();
+        $("#position").empty();
+        $("#interests").empty();
+        $("#favSpot").empty();
+        $("#addSongs").empty();
   
-
-          var currentKey = $(this).attr("data-key");
+        var currentKey = $(this).attr("data-key");
           console.log(currentKey);
 
+        database.ref('users/' + currentKey).on('value', function(data) {
 
-      
+          $("#cardTitle1").append("Name: " + data.val().user_name);
+          $("#email").append("Email: " + data.val().user_email);
+          $("#position").append("Position: " + data.val().user_position);
+          $("#interests").append("Interest: " + data.val().user_interest);
+          $("#favSpot").append("Favorite Spot: " + data.val().user_fav_spot);
 
-          database.ref('users/' + currentKey).on('value', function(data) {
-
-                $("#cardTitle1").append("Name: " + data.val().user_name);
-                $("#email").append("Email: " + data.val().user_email);
-                $("#position").append("Position: " + data.val().user_position);
-                $("#interests").append("Interest: " + data.val().user_interest);
-                $("#favSpot").append("Favorite Spot: " + data.val().user_fav_spot);
-            // if(arron.val()) {
-            //   console.log(arron.val());
-            // }
 
           for (var i=0; i < data.val().user_fav_songs.length; i++) {
             $("#addSongs").append(data.val().user_fav_songs[i] + "<br>");
           }
 
+          event.preventDefault();
 
           });
+    });
 
 
+// ------------------------------------------------------------------------------------------------
 
-
-      });
+    function results() {
   
-  })
+          var artistInput = $("#query").val().trim();
+                
+          // http://app.ticketmaster.com/discovery/v1/events.json?keyword=ariana&apikey=zyleKSfADiALla0NEoeit7ieP42ITTfA&callback=myFunction
+
+          var queryURL = "http://app.ticketmaster.com/discovery/v1/events.json?keyword="+ artistInput +"&apikey=zyleKSfADiALla0NEoeit7ieP42ITTfA&callback=myFunction"
+
+          $.ajax({
+              url: queryURL,
+              method: "GET"
+             }).done(function(response) {
+
+              var div = $("<div>");
+
+              for (var i = 0; i < response._embedded.events.length; i++) {
+                var event = response._embedded.events[i].name;
+                var city = response._embedded.events[i]._embedded.venue[0].city.name;
+                var state = response._embedded.events[i]._embedded.venue[0].state.stateCode;
+                div.append(i+1 + ". " + event + " in " + city + ", " + state + "<br>");
+              }
+
+              $("#addEvents").html("<p>Upcoming performances</p>");
+
+              $("#addEvents").append(div);
+
+              // return false;
+
+            });
+
+      return false;
+
+      };
+
+  $(document).on("click", "#search", results);
+
+// ------------------------------------------------------------------------------------------------
+  
+  });
 
 
 
-
-    // Firebase watcher + initial loader 
     database.ref('/users').on("child_added", function(childSnapshot) {
- 
-      // $('.collection').append('<a href="#" class="collection-item" data-key="'+childSnapshot.key+'"></a>'+childSnapshot.val().user_name)
 
-      // console.log(childSnapshot.val().user_name);
-      // console.log("Data" + childSnapshot.key);
-      // console.log(childSnapshot.val());
-      // console.log(childSnapshot.val().user_email);
-      // console.log(childSnapshot.val().user_position);
-      // console.log(childSnapshot.val().user_interest);
-      // console.log(childSnapshot.val().user_fav_spot);
-
-
-
-
-
-
-
-
-      $("#list").append("<button type='button' class='list-group-item' data-key=" + childSnapshot.key + ">" + childSnapshot.val().user_name + "</button>");
-      // $("#list").append("<button type='button' class='list-group-item'>" + childSnapshot.key + "</button>");
-
-
-
-     
-      
+      $("#list").append("<button id='listButtons' type='button' class='list-group-item' data-key=" + childSnapshot.key + ">" + childSnapshot.val().user_name + "</button>");
 
     // Handle the errors
     }, function(errorObject) {
